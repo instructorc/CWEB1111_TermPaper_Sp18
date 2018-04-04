9e27db976869ba256593dae8a5a84248f5fb3972
cifulton <cfulton@dunwoody.edu>
2018-04-04 09:26:47 -0500

    initial commit

--- Termpaper[Sp18]/Program.cs
+++ Termpaper[Sp18]/Program.cs
@@ -10,6 +10,58 @@ namespace Termpaper_Sp18_
     {
         static void Main(string[] args)
         {
+            string name, subject, letterGrade;
+
+            TermPaper aPaper = new TermPaper();
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            aPaper.setAuthorsName(name);
+
+
+        }
+    }
+
+    class TermPaper
+    {
+        private string authorsName;
+        private string subject;
+        private string letterGrade;
+
+
+        //public TermPaper(string authorsName, string subject, string letterGrade)
+        //{
+        //    this.authorsName = authorsName;
+        //    this.subject = subject;
+        //    this.letterGrade = letterGrade;
+
+        //}
+
+        /**Setter methods**/
+        public void setAuthorsName(string name)
+        {
+            authorsName = name;
+        }
+       
+        public void setSubject(string sub)
+        {
+            subject = sub;
+        }
+        public void setLetterGrade(string grade)
+        {
+            letterGrade = grade;
+        }
+
+        public string getAuthorsName()
+        {
+            return authorsName;
+        }
+        public string getSubject()
+        {
+            return subject;
+        }
+        public string getGrade()
+        {
+            return letterGrade;
         }
     }
 }
