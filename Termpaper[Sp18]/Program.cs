2804a82cd21d9ebf1cd8f12bda0eb7e2f87a208e
Fulton <fulchr@dunwoody.edu>
2018-04-04 10:56:55 -0500

    adding object of arrays, constructor, tostring(), and other stuff

--- Termpaper[Sp18]/Program.cs
+++ Termpaper[Sp18]/Program.cs
@@ -10,14 +10,31 @@ namespace Termpaper_Sp18_
     {
         static void Main(string[] args)
         {
+            //Global variables
             string name, subject, letterGrade;
+           
+            TermPaper[] aPaper = new TermPaper[2];
 
-            TermPaper aPaper = new TermPaper();
-            Console.WriteLine("Please enter your name");
-            name = Console.ReadLine();
-            aPaper.setAuthorsName(name);
+            for (var x = 0; x < aPaper.Length; x++)
+            {
+                
+                Console.WriteLine("Please enter the authors name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Please enter the authors subject:");
+                subject = Console.ReadLine();
+                Console.WriteLine("Please enter a letter grade:");
+                letterGrade = Console.ReadLine();
+
+                aPaper[x] = new TermPaper(name, subject, letterGrade);
 
 
+            }
+            Console.WriteLine("");
+            for(var x=0; x < aPaper.Length; x++)
+            {
+                Console.WriteLine(aPaper[x]);
+            }
+            Console.WriteLine("Program has concluded");
         }
     }
 
@@ -27,29 +44,34 @@ namespace Termpaper_Sp18_
         private string subject;
         private string letterGrade;
 
+        //Default Constructor
+        public TermPaper()
+        {
 
-        //public TermPaper(string authorsName, string subject, string letterGrade)
-        //{
-        //    this.authorsName = authorsName;
-        //    this.subject = subject;
-        //    this.letterGrade = letterGrade;
+        }
+        //Overloaded Constructor that takes in 3 parameters
+        public TermPaper(string authorsName, string subject, string letterGrade)
+        {
+            this.authorsName = authorsName;
+            this.subject = subject;
+            this.letterGrade = letterGrade;
 
-        //}
+        }
 
         /**Setter methods**/
-        public void setAuthorsName(string name)
-        {
-            authorsName = name;
-        }
+        //public void setAuthorsName(string name)
+        //{
+        //    authorsName = name;
+        //}
        
-        public void setSubject(string sub)
-        {
-            subject = sub;
-        }
-        public void setLetterGrade(string grade)
-        {
-            letterGrade = grade;
-        }
+        //public void setSubject(string sub)
+        //{
+        //    subject = sub;
+        //}
+        //public void setLetterGrade(string grade)
+        //{
+        //    letterGrade = grade;
+        //}
 
         public string getAuthorsName()
         {
@@ -62,6 +84,10 @@ namespace Termpaper_Sp18_
         public string getGrade()
         {
             return letterGrade;
+        }
+        public override string ToString()
+        {
+            return "The author: " + authorsName + " The subject: " + subject + " The grade: " + letterGrade;
         }
     }
 }
